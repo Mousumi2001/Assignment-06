1d6a303f6f35027cb50cbf5e2debbec93e2e8922
Mousumi2001 <am6555264@gmail.com>
2022-09-04 07:51:36 +0600

    js code

--- js/news.js
+++ js/news.js
@@ -27,5 +27,41 @@ const displayCard = cards => {
     const cardContainer = document.getElementById('card');
     cards.forEach(card => {
         // console.log(card)
+        const cardDiv = document.createElement('div');
+        cardDiv.classList.add('row');
+        cardDiv.classList.add('border-0');
+        cardDiv.classList.add('shadow');
+        cardDiv.classList.add('mb-5');
+
+        cardDiv.innerHTML = `
+            <div class="col-md-3  col-xs-6 p-0 border-0">
+                            <img src="${card.thumbnail_url}" class="img-fluid rounded-start p-2" alt="...">
+                        </div>
+                        <div class="col-md-9 col-xs-12 p-0 border-0">
+                            <div class="card-body">
+                                <h5 class="card-title">${card.title}</h5>
+                                <p class="card-text">${card.details}</p>                                
+                            </div>
+                            <div class="d-flex justify-content-around align-items-center">
+                              <div class="d-flex">
+                              <div >
+                                <img class="img-fluid rounded-circle border border-2" src="...">
+                              </div>
+                                <div>
+                                    <p class="fs-6">${card.author.published_date}</p >
+                                    <p class="fs-6">${card.author.name}</p >
+                                 </div>
+                                 </div>
+                                 <div>
+                                 <p>view: ${card.rating.number}M</p>
+                                 </div>
+                                 <div>
+                                 <button onclick="modalDetails()" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">click</button>
+                                 
+                                 </div>
+                            </div>
+                        </div >
+    `;
+        cardContainer.appendChild(cardDiv);
     })
 }
