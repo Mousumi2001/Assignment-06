f4cf2012eb7c9c7722634171b8dfb83c8f857ccb
Mousumi2001 <am6555264@gmail.com>
2022-09-04 07:48:10 +0600

    js code

--- js/news.js
+++ js/news.js
@@ -3,3 +3,15 @@ function loadUsers2() {
         .then(res => res.json())
         .then(data => displayUsers2(data.data.news_category))
 }
+
+function displayUsers2(data) {
+    const ul = document.getElementById('users-list')
+    for (const user of data) {
+        // console.log(user.category_name);
+        const li = document.createElement('li');
+        li.classList.add('style')
+        li.innerText = user.category_name;
+        ul.appendChild(li)
+    }
+}
+loadUsers2()
