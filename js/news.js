0b426839bd0aa2626b2c762b56e6d839b93ac31a
Mousumi2001 <am6555264@gmail.com>
2022-09-04 10:25:26 +0600

    change file name

--- js/news.js
+++ js/news.js
@@ -2,6 +2,7 @@ function loadUsers2() {
     fetch(' https://openapi.programming-hero.com/api/news/categories')
         .then(res => res.json())
         .then(data => displayUsers2(data.data.news_category))
+        .catch(error => console.log(error))
 }
 
 function displayUsers2(data) {
