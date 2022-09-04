cea426fbcf2c2e925f9b1f90352c5cea5d0e2f96
Mousumi2001 <am6555264@gmail.com>
2022-09-04 07:49:03 +0600

    js code

--- js/news.js
+++ js/news.js
@@ -15,3 +15,10 @@ function displayUsers2(data) {
     }
 }
 loadUsers2()
+
+const loadCard = () => {
+    const url = `https://openapi.programming-hero.com/api/news/category/01`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayCard(data.data))
+}
